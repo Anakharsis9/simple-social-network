a1694f990829638f0e4042db89682b42f6e6a67e
Dayana <dayana530807@gmail.com>
2022-02-11 18:34:07 +0300

    project initialization

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  created() {},
+};
+</script>
 
 <style lang="scss">
 #app {
